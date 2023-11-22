e5e6c19f85e820808899e5fdb0a3a2dea9494da1
Hassan Ansari <hassanansari222@gmail.com>
2023-11-22 16:05:28 +0530

    Deaker App Updates

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -5,7 +5,13 @@ class Constants {
   static bool get isDev => const bool.fromEnvironment('isDev');
 
   /// This is environment string based on isDev boolean
-  static String environment = isDev ? 'development' : 'production';
+  static String environment =
+      isDev ? 'dealer_development' : 'dealer_production';
+
+  /// This is the url for glitchtip
+  /// Used for tracking bugs
+  static String get glitchTipDSN =>
+      const String.fromEnvironment('glitchTipDSN');
 
   /// This is the base url for the api
   static const String seen = 'seen';
