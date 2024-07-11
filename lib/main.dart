5f890ad85843093e807fef4d8c0e1569b81c2530
Ridapatel990 <patelrida990@gmail.com>
2024-07-11 12:30:13 +0530

    changes 11.7.24

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,16 @@ import 'package:dealerapp/src/app/repository/graphql_client.dart';
 import 'package:dealerapp/src/dealer_app.dart';
 import 'package:dealerapp/src/utils/constants.dart';
 import 'package:dealerapp/src/utils/global_exports.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:stack_trace/stack_trace.dart' as stack_trace;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+  // await Firebase.initializeApp();
+  // FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
   setupLocator();
+
   /// Initialize the Cache Provider
   await cacheProvider.initHive();
 
@@ -36,7 +35,8 @@ void main() async {
     appRunner: () async {
       /// Main Entry point of the Flutter App
       ///
-      FirebaseAnalytics.instance.logEvent(name: 'increment_button_press');
+      // FirebaseAnalytics.instance.logEvent(name: 'increment_button_press');
+
       /// Setup DI with GetIt
       /// [ProviderScope] is the root of the Riverpod
       /// for initializing all the providers
